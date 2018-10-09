39dda3170ac97db2a1cdd5d25d19bdf3946c5596
Daniel Vaknine <daniel.vaknine@gmail.com>
2018-10-09 11:05:53 +0200

    first_daniel_vaknine

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+var fs = require("fs");
 
 app.use(express.static('dist'))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
+})
+app.get('/api/photos', function (req, res) {
+    let strData = fs.readFile("urls.txt", "utf-8", function (err, data) {
+        let photos = [];
+        let tmp = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data.charCodeAt(i) == 10) {
+                let photo = {
+                    id: photos.length,
+                    title: "",
+                    url: tmp.join('')
+                }
+                // console.log(tmp.join(''));
+                tmp = [];
+                photos.push(photo);
+            } else {
+                tmp.push(data.charAt(i));
+            }
+        }
+        res.send(photos);
+    })
+})
+
+app.post('/api/photos/update', function (req, res) {
+    // let photos = JSON.parse(req.body.photos);
+    console.log("updateing");
+    res.send({ message: "Updated" });
 })
 
 app.listen(3000, () => console.log('App listening on port 3000'))
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,134 @@
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+
+  data() {
     return {
-      msg: 'Hello world'
+      msg: "Bildarkivet",
+      photos: []
+    };
+  },
+  created: function() {
+    fetch("http://localhost:3000/api/photos/", {
+      credentials: "same-origin"
+    }).then(res =>
+      res.json().then(res => {
+        console.log(res);
+        this.photos = res;
+      })
+    );
+  },
+  methods: {
+    updatePhoto: function(photo) {
+      let header = new Headers();
+      header.append("Content-Type", "application/json");
+      if (photo.title.length == 0) {
+        alert("Please write something first");
+      } else {
+        fetch("http://localhost:3000/api/photos/update", {
+          credentials: "same-origin",
+          headers: header,
+          method: "POST",
+          body: JSON.stringify(photo)
+        }).then(res =>
+          res.json().then(res => {
+            //this.photo = res;
+            alert(res.message);
+          })
+        );
+      }
     }
   }
-}
+};
 </script>
 
 <template>
   <div id="app">
+    <div class="top">
     <h1>{{msg}}</h1>
+      </div>
+    
+    <div class="photo" v-for="photo in photos">
+      <img :src='photo.url'>
+      <h2>{{photo.title}}</h2>
+      <input v-model="photo.title" placeholder="...">
+      <button v-on:click="updatePhoto(photo)">Submit</button>
+    </div>
+  
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background-color: rgb(41, 37, 37);
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
 }
 
-h1, h2 {
+.top {
+  background-color: white;
+  width: 800px;
+  height: 100px;
+  text-align: center;
+  padding: 10px;
+}
+.photo {
+  background-color: white;
+  width: 800px;
+  height: 800px;
+  text-align: center;
+  margin-top: 20px;
+}
+.photo img {
+  width: 800px;
+  height: 600px;
+  object-fit: cover;
+}
+
+.photo input,
+button {
+  width: 400px;
+  height: 30px;
+  border-radius: 5px;
+  /*border: 1px soild rgba(238, 248, 248, 0.918); */
+  text-align: center;
+  border-style: none;
+  display: inline-block;
+}
+
+.photo button {
+  color: rgb(83, 83, 83);
+  font-size: 100%;
+  background-color: rgba(238, 248, 248, 0.918);
+}
+
+.photo button:hover {
+  box-shadow: 3px 3px 3px 3px rgba(155, 152, 152, 0.829);
+  transition: 1s;
+}
+
+.photo input:hover {
+  background-color: rgb(224, 255, 229);
+  transition: 1s;
+}
+
+* {
+  box-sizing: border-box;
+}
+h1,
+h2 {
   font-weight: normal;
+  color: black;
 }
 
 ul {
